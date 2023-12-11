b83afbb81bb765822f3eca098c9ba2f02afb1bd6
Max Lyuchin <maxim.lyuchin@gmail.com>
2023-12-11 22:22:07 +0300

    Day 11 b? :(

--- 2023/day11.js
+++ 2023/day11.js
@@ -10,27 +10,40 @@ const printUniverse = (universe) => {
   });
 };
 
-const distance = (g1, g2) => {
-  return (
-    Math.abs(galaxies[g1][0] - galaxies[g2][0]) +
-    Math.abs(galaxies[g1][1] - galaxies[g2][1])
-  );
+const distance = (g1, g2) =>
+  Math.abs(galaxies[g1][0] - galaxies[g2][0]) +
+  Math.abs(galaxies[g1][1] - galaxies[g2][1]);
+
+const distance3 = (g1, g2) => {
+  const row = universe[galaxies[g1][0]].join("");
+  const col = transuniverse[galaxies[g2][1]].join("");
+  const xx = [galaxies[g1][0], galaxies[g2][0]];
+  const yy = [galaxies[g1][1], galaxies[g2][1]];
+  xx.sort((a, b) => a - b);
+  yy.sort((a, b) => a - b);
+  const [x1, x2] = xx;
+  const [y1, y2] = yy;
+  const path = col.substring(x1 + 1, x2 + 1) + row.substring(y1 + 1, y2 + 1);
+  const ms = path.replace(/[^M]/g, "");
+  const distance = path.length + ms.length * 9;
+  console.log(g1, g2, path, ms, distance);
+  return distance;
 };
 
 const distance2 = (g1, g2) => {
   let distance = 0;
-  const [x1, y1] = galaxies[g1];
-  const [x2, y2] = galaxies[g2];
-  console.log(`Galaxy ${g1}: ${x1},${y1}`);
-  console.log(`Galaxy ${g2}: ${x2},${y2}`);
+  const xx = [galaxies[g1][0], galaxies[g2][0]];
+  const yy = [galaxies[g1][1], galaxies[g2][1]];
+  xx.sort((a, b) => a - b);
+  yy.sort((a, b) => a - b);
+  const [x1, x2] = xx;
+  const [y1, y2] = yy;
   for (let i = x1 + 1; i <= x2; i++) {
-    const element = universe[x1][y1];
-    console.log(`At ${i},${y1} - ${element}`);
+    const element = universe[i][y1];
     distance += element === "M" ? 10 : 1;
   }
   for (let i = y1 + 1; i <= y2; i++) {
-    const element = universe[x1][i];
-    console.log(`At ${x1},${i} - ${element}`);
+    const element = universe[x2][i];
     distance += element === "M" ? 10 : 1;
   }
   console.log(`Distance ${g1 + 1},${g2 + 1}: ${distance}`);
@@ -73,17 +86,22 @@ for (let i = 0; i < universe.length; i++) {
   }
 }
 
+const transuniverse = universe[0].map((_, colIndex) =>
+  universe.map((row) => row[colIndex])
+);
+
 const distances = {};
 const distances2 = {};
 for (let i = 0; i < galaxies.length; i++) {
   for (let j = 0; j < galaxies.length; j++) {
     if (j > i) {
       distances[`${i + 1},${j + 1}`] = distance(i, j);
-      distances2[`${i + 1},${j + 1}`] = distance2(i, j);
+      distances2[`${i + 1},${j + 1}`] = distance3(i, j);
     }
   }
 }
-printUniverse(universe);
+
+printUniverse(universe, galaxies);
 const score1 = Object.values(distances).reduce((p, v) => p + v, 0);
 console.log("Part 1", score1);
 
