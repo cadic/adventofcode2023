31df2f1a24f85773cc90445e446ebbebf184eebf
Max Lyuchin <maxim.lyuchin@gmail.com>
2023-12-13 12:08:34 +0300

    Day 13b

--- 2023/day13.js
+++ 2023/day13.js
@@ -8,32 +8,39 @@ const transpose = (matrix) => {
   return matrix[0].map((_, i) => matrix.map((row) => row[i]));
 };
 
-const arrayEquals = (a, b) => {
-  return (
-    Array.isArray(a) &&
-    Array.isArray(b) &&
-    a.length === b.length &&
-    a.every((val, index) => val === b[index])
-  );
+const stringDiff = (a, b) => {
+  return a.split("").reduce((p, v, i) => {
+    return p + (v === b[i] ? 0 : 1);
+  }, 0);
 };
 
-const getReflection = (lines) => {
-  console.log(lines);
+const arrayDiff = (a, b) => {
+  if (!Array.isArray(a) || !Array.isArray(b) || a.length !== b.length) {
+    return Infinity;
+  }
+  const diffs = [];
+  for (let i = 0; i < a.length; i++) {
+    diffs.push(stringDiff(a[i], b[i]));
+  }
+  return diffs.reduce((p, v) => p + v, 0);
+};
+
+const ref = (lines, smudge = 0) => {
   for (let i = 1; i < lines.length; i++) {
     const size = i < lines.length / 2 ? i : lines.length - i;
     const left = lines.slice(i - size, i).reverse();
     const right = lines.slice(i, i + size);
-    if (arrayEquals(left, right)) {
+    if (arrayDiff(left, right) === smudge) {
       return i;
     }
   }
   return 0;
 };
 
-const reflections = (pattern) => {
+const reflections = (pattern, smudge = 0) => {
   const rows = pattern.map((a) => a.join(""));
   const columns = transpose(pattern).map((a) => a.join(""));
-  const reflections = [getReflection(rows), getReflection(columns)];
+  const reflections = [ref(rows, smudge), ref(columns, smudge)];
   return reflections;
 };
 
@@ -41,3 +48,8 @@ const score1 = patterns
   .map((pattern) => reflections(pattern))
   .reduce((p, v) => p + v[0] * 100 + v[1], 0);
 console.log("Day 13a", score1);
+
+const score2 = patterns
+  .map((pattern) => reflections(pattern, 1))
+  .reduce((p, v) => p + v[0] * 100 + v[1], 0);
+console.log("Day 13b", score2);
