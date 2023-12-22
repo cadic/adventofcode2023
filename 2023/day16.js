1007845984cca6a9204b5ce552aac2705875a920
Max Lyuchin <maxim.lyuchin@gmail.com>
2023-12-22 15:31:43 +0300

    Day 16 part a-b

--- 2023/day16.js
+++ 2023/day16.js
@@ -23,35 +23,36 @@ const rules = {
   },
 };
 
+const init = () => {
+  for (let i = 0; i < input.length; i++) {
+    for (let ii = 0; ii < input[0].length; ii++) {
+      input[i][ii].e = 0;
+      input[i][ii].d = new Map();
+    }
+  }
+};
+
 const beam = (pos, direction) => {
   const x =
     direction === ">" ? pos[0] + 1 : direction === "<" ? pos[0] - 1 : pos[0];
   const y =
     direction === "v" ? pos[1] + 1 : direction === "^" ? pos[1] - 1 : pos[1];
 
-  console.log(`${direction} from [${pos}] to [${x},${y}]`);
-  question("");
-
   // Beam stops if out of range or cell was travelled all 4 directions.
   if (x < 0 || y < 0 || x >= w || y >= h) {
-    console.log("Reach corner, beam ends");
     return;
   }
 
   if (input[y][x].d.has(direction)) {
-    console.log(input[y][x]);
-    console.log("Been here, beam ends");
     return;
   }
 
   input[y][x].d.set(direction, 1);
   input[y][x].e++;
-  console.log(input[y][x]);
 
   switch (input[y][x].t) {
     case "|":
       if ([">", "<"].includes(direction)) {
-        console.log("Split ^v");
         beam([x, y], "^");
         beam([x, y], "v");
       } else {
@@ -61,7 +62,6 @@ const beam = (pos, direction) => {
 
     case "-":
       if (["v", "^"].includes(direction)) {
-        console.log("Split <>");
         beam([x, y], "<");
         beam([x, y], ">");
       } else {
@@ -71,7 +71,6 @@ const beam = (pos, direction) => {
 
     case "\\":
     case "/":
-      console.log("Meet", input[y][x].t, "reflect");
       beam([x, y], rules[input[y][x].t][direction]);
       break;
 
@@ -83,11 +82,27 @@ const beam = (pos, direction) => {
 };
 
 beam([-1, 0], ">");
-//console.log(input);
-input.map((line) => {
-  console.log(line.map((e) => e.d.size).join(""));
-});
-const score1 = input
-  .map((line) => line.reduce((p, v) => p + (v.e > 0 ? 1 : 0), 0))
-  .reduce((p, v) => p + v, 0);
-console.log(score1);
+
+const score = (a) =>
+  a
+    .map((line) => line.reduce((p, v) => p + (v.e > 0 ? 1 : 0), 0))
+    .reduce((p, v) => p + v, 0);
+const score1 = score(input);
+console.log("Part A", score1);
+
+const energy = [];
+for (let i = 0; i < input.length; i++) {
+  init();
+  beam([-1, i], ">");
+  energy.push(score(input));
+  init();
+  beam([input.length, i], "<");
+  energy.push(score(input));
+  init();
+  beam([i, -1], "v");
+  energy.push(score(input));
+  init();
+  beam([i, input.length], "^");
+  energy.push(score(input));
+}
+console.log(Math.max(...energy));
