fe061a94416f9180e406b3cda329422c591f453b
Max Lyuchin <maxim.lyuchin@gmail.com>
2023-12-24 14:20:41 +0300

    Day 15b

--- 2023/day15.js
+++ 2023/day15.js
@@ -9,3 +9,40 @@ const hash = (line) => {
 
 const score1 = input.reduce((p, v) => p + hash(v), 0);
 console.log(score1);
+
+const boxes = Array.from({ length: 256 }, (e) => []);
+
+input.forEach((line) => {
+  if (-1 !== line.indexOf("=")) {
+    const [label, focal] = line.split("=");
+    const i = hash(label);
+    console.log("Box", i);
+    console.log("Insert", label, focal);
+    const ri = boxes[i].findIndex((l) => l.label === label);
+    if (-1 !== ri) {
+      console.log("Exists at position", ri);
+      boxes[i][ri] = { label: label, focal: focal };
+    } else {
+      console.log("Not Exists, adding");
+      boxes[i].push({ label: label, focal: focal });
+    }
+  } else {
+    const [label, _] = line.split("-");
+    const i = hash(label);
+    console.log("Box", i);
+    console.log("Remove", label);
+    const ri = boxes[i].findIndex((l) => l.label === label);
+    if (-1 !== ri) {
+      console.log("exists, remove");
+      boxes[i].splice(ri, 1);
+    }
+  }
+});
+
+const score2 = boxes.reduce(
+  (p, lenses, i) =>
+    p + lenses.reduce((r, lens, j) => r + (i + 1) * lens.focal * (j + 1), 0),
+  0
+);
+
+console.log(score2);
